63fa7c168f3c6f128fe1ba6c15c87d2bfaab1f1a
badCODERever <ferosekhan2020@gmail.com>
2018-11-08 15:03:22 +0530

    genderfinder.py

--- genderfinder.py
+++ genderfinder.py
@@ -1,13 +1,14 @@
-mycoll={'fero':'M','mahi':'M','buvi':'F'}
+mycoll={'fero':'M','buvi':'F','mahi':'M'}
 while True:
-	print('Enter the name to find the gender or leave blank to exit')
-	name=input()
-	if name=='':
-		break
-	elif name in mycoll:
-		print('gender is '+mycoll[name]+' for '+name)
-	else:
-		print('I dont have ur data please enter u r gender')
-		gender=input()
-		mycoll[name]=gender
-		print('updated')
+    print('Enter the name to check')
+    name=input()
+    if name=='':
+        break
+    if name in mycoll:
+        print('Gender is '+mycoll[name])
+    else:
+        print('I dont\'t have ur Gender Enter the gender')
+        gender=input()
+        mycoll[name]=gender
+        print('updated')
+    
